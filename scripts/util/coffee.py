3ad273f7ab24da2b4e8cdf60ca63ca2cc703dd30
William Högman <william@hogman.me>
2012-02-05 01:12:31 +0100

    Error messages from CoffeeScript are now shown properly.

--- scripts/util/coffee.py
+++ scripts/util/coffee.py
@@ -1,15 +1,15 @@
 from subprocess import Popen,PIPE
+from util.exc import BuildFailedError
 
 
 def coffee(data):
-    p =Popen("coffee -s -p",
-                       stdout=PIPE,stdin=PIPE,stderr=PIPE,shell=True)
+    
+    p =Popen("coffee -s -p",stdout=PIPE,stdin=PIPE,
+             stderr=PIPE,shell=True)
 
     output = p.communicate(data)
     if output[1] != "":
-        print("Error from CoffeeScript")
-        print(output[1])
-        raise RuntimeError("could not build CoffeeScript")
+        raise BuildFailedError("CoffeeScript failed",output[1])
 
     return output[0]
     
