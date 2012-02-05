615988f03ff22dd08ac54fb02bfdd2b89b0db3ef
William Högman <william@hogman.me>
2012-02-05 01:12:03 +0100

    fixed bug with BuildFailedError

--- scripts/util/exc.py
+++ scripts/util/exc.py
@@ -3,4 +3,4 @@ class BuildFailedError(RuntimeError):
     """ Raised if a critical failure occurs in the build process """
     def __init__(self,message="",longmsg=""):
         self.longmsg = longmsg
-        super(RuntimeError).__init__(message)
+        super(RuntimeError,self).__init__(message)
