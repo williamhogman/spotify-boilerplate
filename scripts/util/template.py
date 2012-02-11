c94b70072eadbdf40431adcaff9d8a9a6cd1b3d6
William Högman <william@hogman.me>
2012-02-11 22:16:54 +0100

    added function for constructing app urls to the template system

--- scripts/util/template.py
+++ scripts/util/template.py
@@ -5,6 +5,7 @@ class TemplateRenderer(object):
     def __init__(self):
         self.context = {}
         self._add_fn(self.local_file)
+        self._add_fn(self.app_url)
             
     def _add_fn(self,fn):
         self.context[fn.__name__] = fn
@@ -16,8 +17,18 @@ class TemplateRenderer(object):
     def render(self,template):
         """ Renders the template using the current context"""
         return Template(template).render(**self.context)
-        
+    
+    @property
+    def app_id(self):
+        return self.context["AppName"]["en"].lower()
+
+    def app_url(self,link=None):
+        if link is None:
+            return "sp:{}".format(self.app_id)
+        else: 
+            return "sp:{}:{}".format(self.app_id,link)
+            
     def local_file(self,name):
-        return "sp://{}/{}".format(self.context["AppName"]["en"].lower(),name)
+        return "sp://{}/{}".format(self.app_id,name)
         
 
